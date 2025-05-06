a9052f2dfb3ef8943d06f1ec0183583cd41a78d0
Mastowicz <168667138+Mastowicz@users.noreply.github.com>
2025-05-06 17:04:04 +0200

    Delete htdocs directory

--- htdocs/htdocs/home.php
+++
@@ -1,2 +0,0 @@
-<h1> Home </h1>
-<h3>Sklep kołki i gwoździe</h3>
--- htdocs/logout.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="refresh" content="2;url=index.php"/>
-<?php
-session_start();
-session_destroy();
-echo "jesteś wylogowany<br>";
-?>
