25a00f7ababb901d58b0b44e3738eb63e0575453
Mastowicz <168667138+Mastowicz@users.noreply.github.com>
2025-05-19 11:37:46 +0200

    Add files via upload

---
+++ 127_0_0_1.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Maj 19, 2025 at 11:35 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `r2s`
+--
+CREATE DATABASE IF NOT EXISTS `r2s` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci;
+USE `r2s`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) NOT NULL,
+  `adres` varchar(100) NOT NULL,
+  `opis` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `klienci`
+--
+
+INSERT INTO `klienci` (`id`, `nazwa`, `adres`, `opis`) VALUES
+(2, 'abcdef', 'ul. Dzika 32-600 Oświęcim', '                Firma w procesie likwidacji                '),
+(5, 'kompany', 'ul. Frajerska 32-600 Oświęcim', '???'),
+(25, 'nazwa', 'adres', 'opis');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `operacje`
+--
+
+CREATE TABLE `operacje` (
+  `id` int(11) NOT NULL,
+  `idk` int(11) NOT NULL,
+  `idt` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `operacje`
+--
+
+INSERT INTO `operacje` (`id`, `idk`, `idt`, `data`) VALUES
+(1, 5, 6, '0000-00-00'),
+(2, 2, 6, '2025-04-14'),
+(3, 5, 8, '2025-04-27'),
+(4, 5, 8, '2025-04-16'),
+(5, 2, 6, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `operatorzy`
+--
+
+CREATE TABLE `operatorzy` (
+  `id` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `haslo` varchar(300) NOT NULL,
+  `imie` varchar(50) NOT NULL,
+  `nazwisko` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `operatorzy`
+--
+
+INSERT INTO `operatorzy` (`id`, `login`, `haslo`, `imie`, `nazwisko`, `email`, `status`) VALUES
+(1, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Adam', 'Administracyjny', 'admin@server.pl', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `towary`
+--
+
+CREATE TABLE `towary` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(100) NOT NULL,
+  `cena` int(30) NOT NULL,
+  `ilość` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `towary`
+--
+
+INSERT INTO `towary` (`id`, `nazwa`, `cena`, `ilość`) VALUES
+(6, 'deska', 5, 69),
+(8, 'kloda', 12, 5);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `operacje`
+--
+ALTER TABLE `operacje`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idk` (`idk`),
+  ADD KEY `idt` (`idt`);
+
+--
+-- Indeksy dla tabeli `operatorzy`
+--
+ALTER TABLE `operatorzy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `operacje`
+--
+ALTER TABLE `operacje`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `operatorzy`
+--
+ALTER TABLE `operatorzy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `towary`
+--
+ALTER TABLE `towary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `operacje`
+--
+ALTER TABLE `operacje`
+  ADD CONSTRAINT `operacje_ibfk_1` FOREIGN KEY (`idt`) REFERENCES `towary` (`id`),
+  ADD CONSTRAINT `operacje_ibfk_2` FOREIGN KEY (`idk`) REFERENCES `klienci` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
